307417ea06f16b09f44614261bc8a235e27640c6
Daniil Alabuzhev <90332154+Kuroky79@users.noreply.github.com>
2022-11-05 01:38:33 +0300

    auth

---
+++ android/app/signup.dart
@@ -0,0 +1,72 @@
+import 'main.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget{
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+class _SignUpPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BackgroundSignUp(),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Text('Регистрация аккаунта', style: TextStyle(color: Colors.white, fontSize: 37),),
+                    )
+                  )
+                ],
+              ),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
+class BackgroundSignUp extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.65);
+    blueWave.cubicTo(sw * 0.8, sh * 0.8, sw * 0.5, sh * 0.8, sw * 0.45, sh);
+    blueWave.lineTo(0, sh);
+    blueWave.close();
+    paint.color = Colors.lightBlue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    // Grey
+    Path greyPath = Path();
+    greyPath.lineTo(sw, 0);
+    greyPath.lineTo(sw, sh * 0.3);
+    greyPath.cubicTo(sw * 0.65, sh * 0.45, sw * 0.25, sh * 0.35, 0, sh * 0.5);
+    greyPath.close();
+    paint.color = Colors.grey.shade800;
+    canvas.drawPath(greyPath, paint);
+  }
+
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
